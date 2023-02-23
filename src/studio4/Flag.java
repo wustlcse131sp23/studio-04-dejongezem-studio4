44c141b2fb67f53996729150529844e21f75776f
User <User@DESKTOP-KVPGQOL.dhcp.wustl.edu>
2023-02-23 12:34:51 -0600

    studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,30 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
 		
+		Color c = new Color(255, 247, 90);
+		StdDraw.setPenColor(c);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.3);
+		
+		c = new Color(20, 120, 200);
+		StdDraw.setPenColor(c);
+		double[] x = {0.5, 0.25, 0.25};
+		double[] y = {0.5, 0.4, 0.6};
+		StdDraw.filledPolygon(x, y);
+		
+		c = new Color(180, 9, 140);
+		StdDraw.setPenColor(c);
+		double[] x2 = {0.5, 0.75, 0.75};
+		double[] y2 = {0.5, 0.4, 0.6};
+		StdDraw.filledPolygon(x2, y2);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,7 @@
 package studio4;
 
 import java.io.File;
+import java.awt.Color;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -20,5 +21,67 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		
+		boolean filled = in.nextBoolean();
+		
+		Color c = new Color(r, g, b);
+		StdDraw.setPenColor(c); //or use a custom color here!
+		
+		if (shape.equals("triangle") == false)
+		{
+		double par1 = in.nextDouble();
+		double par2 = in.nextDouble();
+		double par3 = in.nextDouble();
+		double par4 = in.nextDouble();
+
+	
+			if (filled && shape.equals("rectangle"))
+			{
+				StdDraw.filledRectangle(par1, par2, par3, par4);
+			}
+			else if (!filled && shape.equals("rectangle"))
+			{
+				StdDraw.rectangle(par1, par2, par3, par4);
+				
+			}
+			else if (filled && shape.equals("ellipse"))
+			{
+				StdDraw.filledEllipse(par1, par2, par3, par4);
+			}
+			else if (!filled && shape.equals("ellipse"))
+			{
+				StdDraw.ellipse(par1, par2, par3, par4);
+			}
+			
+		}
+		else
+		{
+			double x1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double y3 = in.nextDouble();
+			
+			double[] x = {x1, x2, x3};
+			double[] y = {y1, y2, y3};
+			
+			if (filled)
+			{
+				StdDraw.filledPolygon(x, y);
+			}
+			else 
+			{
+				StdDraw.polygon(x, y);
+			}
+		}
+		
+		
+		 
 	}
 }
